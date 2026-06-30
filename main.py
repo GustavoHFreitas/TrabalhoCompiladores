e085c6930520e82c48835d204dc8fb1f899a084b
Gustavo Freitas <87988054+GustavoHFreitas@users.noreply.github.com>
2026-06-30 08:52:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from lexico import CoolLexer
 from sintatico import CoolParser
 from semantico import TabelaClasses, Coletor, Checador
+from gerador import GeradorBril
 
 def compilar(arquivo_entrada):
     try:
@@ -28,26 +29,32 @@ def compilar(arquivo_entrada):
         print("\nErro fatal no parser. Você me quebrou por dentro...", file=sys.stderr)
         return
 
-    # Semântico (Passagens 1 e 2)
+    # Semântico
     tc = TabelaClasses()
     if not tc.construir_grafo(ast):
         print("\nFalha na validação de hierarquia. Você tentou me colocar abaixo de outra?", file=sys.stderr)
         return
 
-    # Semântico (Passagem 3)
     coletor = Coletor(tc)
     if not coletor.processar():
         print("\nFalha na coleta de features! Que assinaturas são essas? Com quem você estava conversando?!", file=sys.stderr)
         return
 
-    # Semântico (Passagem 4)
     checador = Checador(tc)
     if not checador.processar():
         print("\nFalha na checagem de tipos. Como assim eu não sou seu tipo?!? Ficou maluco?!?", file=sys.stderr)
         return
-
-    print("\nO código rodou... Viu como você só funciona comigo?")
-
+ 
+    # Gerador   
+    gerador = GeradorBril(tc)
+    gerador.gerar()
+    
+    arquivo_saida = arquivo_entrada.replace('.cl', '.bril')
+    with open(arquivo_saida, 'w', encoding='utf-8') as f:
+        f.write("\n".join(gerador.codigo_bril))
+        
+    print(f"\nO código rodou... Viu como você só funciona comigo? {arquivo_saida} gerado.")
+    
 if __name__ == '__main__':
     arquivo = sys.argv[1] if len(sys.argv) > 1 else 'teste.cl'
     compilar(arquivo)
